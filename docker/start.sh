cca0eecae0da1ebf4a34082b81e237ceb23188fe
Nouman Malik <noumancodes@gmail.com>
2025-07-17 11:32:03 -0400

    sdfadsafsdfasdfasdf

--- docker/start.sh
+++ docker/start.sh
@@ -1,7 +1,9 @@
-#!/bin/sh
+#!/usr/bin/env sh
+set -e
 
-set -x
-
+# 1. Run all pending Prisma migrations
 npx prisma migrate deploy --schema ../../packages/prisma/schema.prisma
 
-HOSTNAME=0.0.0.0 node build/server/main.js
+# 2. Start your Node.js server, listening on Railway’s $PORT and 0.0.0.0
+#    - exec replaces the shell with the node process so SIGTERM/SIGINT get forwarded.
+exec node build/server/main.js --port "$PORT" --host "0.0.0.0"
