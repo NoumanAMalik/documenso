6954e9785079ad9d33526787ecc6ad4086771222
Nouman Malik <noumancodes@gmail.com>
2025-07-17 11:24:16 -0400

    sdfadaf

--- docker/start.sh
+++ docker/start.sh
@@ -4,18 +4,4 @@ set -x
 
 npx prisma migrate deploy --schema ../../packages/prisma/schema.prisma
 
-# Try to find the correct server build file
-if [ -f "build/server/index.js" ]; then
-    echo "Starting server with build/server/index.js"
-    HOSTNAME=0.0.0.0 node build/server/index.js
-elif [ -f "build/index.js" ]; then
-    echo "Starting server with build/index.js"
-    HOSTNAME=0.0.0.0 node build/index.js
-else
-    echo "Error: Could not find server build file"
-    echo "Contents of build directory:"
-    ls -la build/ || echo "build directory not found"
-    echo "Contents of current directory:"
-    ls -la ./
-    exit 1
-fi
+HOSTNAME=0.0.0.0 node build/server/main.js
